44de5649ed47c5a1812198034b78230e21aa4c21
Aaron Handleman <aaro1996@gmail.com>
2016-11-02 18:38:44 -0500

    started formatting code to fit style

--- combined_scripts.js
+++ combined_scripts.js
@@ -1,10 +1,28 @@
 var socketio = io.connect();
-socketio.on("message_to_client",function(data) {
-   //Append an HR thematic break and the escaped HTML of the new message
-   document.getElementById("chatlog").appendChild(document.createElement("hr"));
-   document.getElementById("chatlog").appendChild(document.createTextNode(data['message']));
-});
-function sendMessage(){
-   var msg = document.getElementById("message_input").value;
-   socketio.emit("message_to_server", {message:msg});
+var my_script = {
+   div_chatlog: document.getElementById("div_chatlog"),
+   button_send_message: document.getElementById("button_send_message"),
+   input_text_message: document.getElementById("input_text_message")
+
+};
+socketio.on("message_to_client",
+   function(data) {
+      //Append an HR thematic break and the escaped HTML of the new message
+      set_new_message(data['message'], my_script.div_chatlog);
+   }
+);
+function set_new_message(msg, container) {
+   container.appendChild(document.createElement("hr"));
+   container.appendChild(document.createTextNode(msg));
 }
+
+function send_message(msg){
+   socketio.emit("send_chat", {message:msg});
+}
+
+
+
+//Event Listeners
+my_script.button_send_message.addEventListener("click", function(ev){
+   send_message(my_script.input_text_message.value);
+})

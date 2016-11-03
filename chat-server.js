e27113435d033dccb72e7b6dd7aa3a4076ceda27
Aaron Handleman <aaro1996@gmail.com>
2016-11-03 13:44:59 -0500

    maybe I can host static files now?

--- chat-server.js
+++ chat-server.js
@@ -1,24 +1,52 @@
 //Base code from wiki
 
-// Require the packages we will use:
-var http = require("http"),
+
+// Require the functionality we need to use:
+var http = require('http'),
+	url = require('url'),
+	path = require('path'),
+	mime = require('mime'),
+	path = require('path'),
 	socketio = require("socket.io"),
-	combined_scripts = require("combined_scripts"),
-	fs = require("fs");
+	fs = require('fs');
  
-// Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html:
+// Make a simple fileserver for all of our static content.
+// Everything underneath <STATIC DIRECTORY NAME> will be served.
 var app = http.createServer(function(req, resp){
-	// This callback runs when a new connection is made to our HTTP server.
+	var filename = path.join(__dirname, "static", url.parse(req.url).pathname);
+	(fs.exists || path.exists)(filename, function(exists){
+		if (exists) {
+			fs.readFile(filename, function(err, data){
+				if (err) {
+					// File exists but is not readable (permissions issue?)
+					resp.writeHead(500, {
+						"Content-Type": "text/plain"
+					});
+					resp.write("Internal server error: could not read file");
+					resp.end();
+					return;
+				}
  
-	fs.readFile("client.html", function(err, data){
-		// This callback runs when the client.html file has been read from the filesystem.
- 
-		if(err) return resp.writeHead(500);
-		resp.writeHead(200);
-		resp.end(data);
+				// File exists and is readable
+				var mimetype = mime.lookup(filename);
+				resp.writeHead(200, {
+					"Content-Type": mimetype
+				});
+				resp.write(data);
+				resp.end();
+				return;
+			});
+		}else{
+			// File does not exist
+			fs.readFile("client.html", function(err, data){
+				// This callback runs when the client.html file has been read from the filesystem.
+				if(err) return resp.writeHead(500);
+				resp.writeHead(200);
+				resp.end(data);
+			});
+		}
 	});
 });
-app.listen(3456);
  
 // Do the Socket.IO magic:
 var io = socketio.listen(app);

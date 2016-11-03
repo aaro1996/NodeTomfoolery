e2b96ad90e780284d7b48d9e1cbe54680d8b5ff4
Aaron Handleman <aaro1996@gmail.com>
2016-11-03 14:14:18 -0500

    probably works now?

--- chat-server.js
+++ chat-server.js
@@ -15,8 +15,7 @@ var http = require('http'),
 var app = http.createServer(function(req, resp){
 	var filename = path.join(__dirname, "static", url.parse(req.url).pathname);
 	(fs.exists || path.exists)(filename, function(exists){
-		if (exists) {
-			console.log(filename);
+		if (exists && url.parse(req.url).pathname == "my_script.js") {
 			fs.readFile(filename, function(err, data){
 				if (err) {
 					// File exists but is not readable (permissions issue?)

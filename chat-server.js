f02ab304b59da0fc86794143073a5bd69e8cffb1
Aaron Handleman <aaro1996@gmail.com>
2016-11-14 01:40:38 -0600

    MORE THINGS WORK MORE THAN BEFORE

--- chat-server.js
+++ chat-server.js
@@ -47,22 +47,206 @@ var app = http.createServer(function(req, resp){
 		}
 	});
 });
+
 app.listen(3456); 
 // Do the Socket.IO magic:
+var room_info_object = {};
+room_info_object['lobby'] = {name: 'lobby', cur_users: {}, password: false};
+room_info_object['lobby']['banned_list'] = {};
+var username_info_object = {};
+var socket_id_to_username = {};
 var io = socketio.listen(app);
 var cur_date = new Date();
 io.sockets.on("connection", function(socket){
 	// This callback runs when a new Socket.IO connection is established.
- 
-	socket.on('send_message', function(data) {
-		// This callback runs when the server receives a new message from the client.
- 		console.log("message in " + data['room'] + " by " + data['username'] + " at " + data['time'] + ": "+data['message']); // log it to the Node.JS output
+	socket_id_to_username[socket.id] = true;
+ 	socket.on('join_room_request', function(data) {
+ 		console.log("join_room_request: " + data['room']);
+ 		if (!room_info_object[data['room']]) {
+ 			console.log("failed?");
+ 			return;
+ 		}
+ 		if (verify_username_socket_link(data['username'], socket.id)) {
+ 			var send_object = {};
+ 			if(room_info_object[data['room']]['banned_list'][data['username']]) {
+ 				send_object['success'] = false;
+ 				send_object['fail_message'] = "You're banned from this room!";
+ 				socket.emit("join_room_request_response", send_object);
+ 			}
+ 			if (room_info_object[data['room']]['password'] !== data['password']) {
+ 				send_object['success'] = false;
+ 				send_object['fail_message'] = "Wrong password";
+ 				socket.emit("join_room_request_response", send_object);
+ 			}
+ 			username_socket_leave_current_room(data['username'], socket);
+ 			send_object['admin_rights'] = (room_info_object[data['room']]['admin'] == data['username']);
+ 			send_object['room'] = data['room'];
+ 			send_object['success'] = true;
+ 			console.log("Added " + data['username'] + " to room: " + data['room']);
+ 			username_socket_join_room(data['username'], data['room'], socket);
+ 			socket.emit("join_room_request_response", send_object);
+ 		}
+ 	});
+
+
+	socket.on("user_list_request", function(data) {
+		if (!verify_username_socket_room_link(data['username'], socket.id, data['room'])) {
+			return;
+		}
+		for (user in room_info_object[data['room']]['cur_users']) {  //Adds the current users into the new user's list of users in the room
+ 			var send_object = {};
+ 			send_object['operation'] = 1;
+			send_object['added_user'] = user;
+			socket.emit("userlist_edit", send_object);
+ 		}
+	});
+
+
+ 	socket.on('create_room_request', function(data) {
  		var send_object = {};
- 		send_object['time'] = data['time'];
+ 		if (room_info_object[data['room']]) {
+ 			send_object['success'] = false;
+ 			send_object['error'] = "There is already a room by this name";
+ 			console.log("Attempted to create existing room");
+ 			return;
+ 		}
+ 		if (!verify_username_socket_link(data['username'], socket.id)) {
+ 			send_object['success'] = false;
+ 			send_object['error'] = "There is an issue with your login";
+ 			console.log("Attempted to create room with failed login");
+ 			return;
+ 		}
+ 		room_info_object[data['room']] = {name: data['room'], cur_users: {}, password: data['password'], admin: data['username']};
+ 		send_object['success'] = true;
  		send_object['room'] = data['room'];
- 		send_object['username'] = data['username'];
- 		send_object['message'] = data['message'];
- 		io.sockets.emit("message_to_client", send_object) // broadcast the message to other users
+ 		socket.emit("create_room_request_response", send_object);
+ 		io.sockets.emit("add_room", {room: data['room'], password: !!data['password']});
+ 	});
+
+ 	socket.on('disconnect', function(data) {
+ 		if (socket_id_to_username[socket.id]) {
+ 			if (socket_id_to_username[socket.id] === true) {
+ 				console.log("unregisted user disconnected")
+ 				delete socket_id_to_username[socket.id];
+ 			} else {
+ 				console.log(socket_id_to_username[socket.id] + " has disconnected");
+ 				username_socket_leave_current_room(socket_id_to_username[socket.id], socket);
+ 				delete username_info_object[socket_id_to_username[socket.id]];
+ 				delete socket_id_to_username[socket.id];
+ 			}
+ 		}
+ 	});
+
+
+
+
+ 	socket.on('username_request', function(data) {
+ 		var send_object = {};
+ 		console.log("username_request: " + data['req_username']);
+ 		if (!username_info_object[data['req_username']]) {
+ 			username_info_object[data['req_username']] = {s_id: socket.id, room: false};
+ 			socket_id_to_username[socket.id] = data['req_username'];
+ 			send_object['accepted'] = true;
+ 			send_object['username_set'] = data['req_username'];
+ 			if (data['cur_username']) {
+ 				delete username_info_object[data['cur_username']];
+ 			}
+ 		} else {
+ 			send_object['accepted'] = false;
+ 			send_object['extra_message'] = "That screen name is taken, pick another please.";
+ 		}
+ 		socket.emit("username_request_response", send_object);
+ 	});
+
+
+
+	socket.on('send_room_message', function(data) {
+		// This callback runs when the server receives a new message from the client.
+ 		var send_object = {};
+ 		if (verify_username_socket_room_link(data['username'], socket.id, data['room'])) {
+ 			console.log("message in " + data['room'] + " by " + data['username'] + " at " + data['time'] + ": "+ data['message']); 
+ 			send_object['room'] = data['room'];
+ 			send_object['time'] = data['time'];
+ 			send_object['username'] = data['username'];
+ 			send_object['message'] = data['message'];
+ 			io.sockets.to(data['room']).emit("message_to_client", send_object); // broadcast the message to other users in the given room
+ 		} else {
+ 			console.log("failed message in " + data['room'] + " by " + data['username'] + " at " + data['time'] + ": "+ data['message']);
+ 			send_error("username_socket_room_error", socket);
+ 		}
 	});
 });
+function username_socket_leave_current_room(username, socket) {
+	if (!verify_username_socket_link(username, socket.id)) {
+		return;
+	}
+	if (username_info_object[username]['room']) {
+		username_socket_leave_room(username, username_info_object[username]['room'], socket);
+	}
+};
 
+function username_socket_leave_room(username, room, socket) {
+	if (!verify_username_socket_room_link(username, socket.id, room)) {
+		return;
+	}
+	var socket_id = socket.id;
+	username_info_object[username]['room'] = false;
+	socket.leave(room);
+	remove_user_from_room(room, username);
+	
+};
+
+function username_socket_join_room(username, room, socket) {
+	if (!verify_username_socket_link(username, socket.id)) {
+		return;
+	}
+	var socket_id = socket.id;
+	username_info_object[username]['room'] = room;
+	socket.join(room);
+	add_user_to_room(room, username);
+	
+};
+
+function remove_user_from_room(room, username) {
+	delete room_info_object[room]['cur_users'][username];
+	var send_object = {};
+	send_object['operation'] = 0; //remove user
+	send_object['removed_user'] = username;
+	io.sockets.to(room).emit("userlist_edit", send_object);
+};
+
+function add_user_to_room(room, username) {
+	room_info_object[room]['cur_users'][username] = true;
+	var send_object = {};
+	send_object['operation'] = 1; //add user
+	send_object['added_user'] = username;
+	io.sockets.to(room).emit("userlist_edit", send_object);
+};
+
+function change_username_in_room(room, username_old, username_new) {
+	delete room_info_object[room]['cur_users'][username_old];
+	room_info_object[room]['cur_users'][username_new] = true;
+	var send_object = {};
+	send_object['operation'] = 2; //edit username
+	send_object['old_username'] = username_old;
+	send_object['new_username'] = username_new;
+	io.sockets.to(room).emit("userlist_edit", send_object);
+};
+
+function verify_username_socket_link(username, socket_id) {
+	if (!username) {
+		//console.log("failed username check")
+		return false;
+	}
+	return (socket_id_to_username[socket_id] == username  && username_info_object[username]['s_id'] == socket_id);
+};
+
+function verify_username_socket_room_link(username, socket_id, room_name) {
+	if (!verify_username_socket_link(username, socket_id)) {
+		//console.log("failed_username_socket_check");
+		return false;
+	}
+	return ((username_info_object[socket_id_to_username[socket_id]]['room'] === room_name) && room_info_object[room_name]['cur_users'][username]);
+};
+
+function send_error(error, socket) {};
--- static/my_script.js
+++ static/my_script.js
@@ -4,49 +4,176 @@ var my_script = {
    element_div_chatlog: document.getElementById("div_chatlog"),
    element_button_send_message: document.getElementById("button_send_message"),
    element_input_text_message: document.getElementById("input_text_message"),
-   current_chatroom: "default",
-   username: "danny"
+   element_user_list: document.getElementById("user_list"),
+   element_room_list: document.getElementById("room_list"),
+   current_chatroom: false,
+   username: false
 };
 
+function send_private_message(username, msg) {
 
-//recieves the messages sent to the client, and checks to see if the current chatroom is set
+};
+
+function send_join_room_request(room_name, password) {
+   var send_object = {};
+   if (room_name) {
+      send_object['room'] = room_name;
+   } else {
+      send_object['room'] = "lobby";
+   }
+   if (password) {
+      send_object['password'] = password;
+   } else {
+      send_object['password'] = false;
+   }
+   send_object['username'] = my_script.username;
+   if (my_script.current_chatroom === send_object['room']) {  //if they try to rejoin their current room it just clears everything because that makes the user think that I did what they wanted even though that's a stupid request and causes issues serverside.
+      reset_user_list();
+      reset_chat_list();
+   } else {
+      socketio.emit("join_room_request", send_object);
+   }
+};
 
 function get_date_string_from_date(raw_date) {
    var in_date = new Date(raw_date);
    var date_string = (in_date.getMonth() + 1) + "/" + in_date.getDate() + "/" + in_date.getFullYear() + " - " + ("00" + in_date.getHours()).slice(-2) + ":" + ("00" + in_date.getMinutes()).slice(-2) + ":" + ("00" + in_date.getSeconds()).slice(-2);
    return date_string;
-}
+};
 
 function set_new_message(msg, container) {
    container.appendChild(document.createTextNode("[" + get_date_string_from_date(msg['time']) + "] " + msg['username'] + ": " + msg['message']));
    container.appendChild(document.createElement("br"));
 };
 
-function send_message(msg){
+function reset_user_list() {
    var send_object = {};
-   if (my_script.username != null) {
+   send_object['username'] = my_script.username;
+   send_object['room'] = my_script.current_chatroom;
+   while(my_script.element_user_list.firstChild) {
+        my_script.element_user_list.removeChild(my_script.element_user_list.firstChild);
+   }
+   socketio.emit("user_list_request", send_object);
+}
+
+function reset_chat_list() {
+   while(my_script.element_div_chatlog.firstChild) {
+        my_script.element_div_chatlog.removeChild(my_script.element_div_chatlog.firstChild);
+   }
+}
+
+function prompt_for_username(extra_message) {
+   var username_prompt;
+   if (typeof(extra_message) === "string") {
+      username_prompt = prompt(extra_message, "Username Here");
+   } else {
+      username_prompt = prompt("Please enter a screen name", "Username Here");
+   }
+   if (username_prompt != "" && username_prompt != "Username Here") {
+      var send_object = {};
+      send_object['cur_username'] = my_script.username;
+      send_object['req_username'] = username_prompt;
+      socketio.emit("username_request", send_object);
+   } else if (username_prompt == "") {
+      setTimeout(prompt_for_username("Please enter a username, don't just leave it blank"), 1);
+   } else if (username_prompt == "Username Here") {
+      setTimeout(prompt_for_username("Please enter a screen name, don't just leave it at the default"), 1);
+   }
+};
+
+function send_room_message(msg){
+   var send_object = {};
+   if (my_script.username != false) {
       send_object['username'] = my_script.username;
       send_object['room'] = my_script.current_chatroom;
       send_object['message'] = msg;
       send_object['time'] = Date.now();
-      socketio.emit("send_message", send_object);
+      socketio.emit("send_room_message", send_object);
    } else {
-      alert("Please choose a screen-name before sending messages!");
+      alert("You don't have a username, try setting one");
    }
 };
 
 
 //Event Listeners
 my_script.element_button_send_message.addEventListener("click", function(ev){
-   send_message(my_script.element_input_text_message.value);
+   send_room_message(my_script.element_input_text_message.value);
+   console.log("sent message");
 });
 
 
-socketio.on("message_to_client",
-   function(data) {
-      //Append an HR thematic break and the escaped HTML of the new message
-      if (data['room'] && data['room'] === my_script.current_chatroom && data['message'] && data['username']) {
-         set_new_message(data, my_script.element_div_chatlog);
-      }
+socketio.on("message_to_client", function(data) {
+   if (data['room'] && data['room'] === my_script.current_chatroom && data['message'] && data['username']) {
+      set_new_message(data, my_script.element_div_chatlog);
    }
-);
+});
+
+
+socketio.on("username_request_response", function(data) {
+   console.log("urr");
+   if (data['accepted'] === true) {
+      my_script.username = data['username_set'];
+      console.log("sending room request");
+      if (!my_script.current_chatroom) {
+         send_join_room_request();
+      }
+      console.log("send room request");
+   } else {
+      prompt_for_username(data['extra_message']);
+   }
+});
+
+socketio.on("userlist_edit", function(data) {
+   switch(data['operation']) {
+      case 0:
+
+      break;
+      case 1: //adds a user to the user list
+         var new_user_element = document.createElement("li");
+         new_user_element.id = "li_user_" + data['added_user'];
+         new_user_element.setAttribute("data-username", data['added_user']);
+         new_user_element.textContent = data['added_user'];
+         my_script.element_user_list.appendChild(new_user_element);
+         new_user_element.addEventListener("click", function(ev) {
+            var message_prompt = prompt("Send a message to " + ev.currentTarget.getAttribute("data-username"), "");
+            send_private_message(ev.currentTarget.getAttribute("data-username"), message_prompt);
+         });
+      break;
+      case 2:
+      break;
+   }
+});
+
+socketio.on("join_room_request_response", function(data) {
+   if(data['success']) {
+      my_script.current_chatroom = data['room'];
+      reset_user_list();
+      reset_chat_list();
+   }
+});
+
+socketio.on("create_room_request_response", function(data) {
+   
+});
+
+socketio.on("add_room", function(data) {
+   var new_room_element = document.createElement("li");
+   new_room_element.id = "li_room_" + data['room'];
+   new_room_element.textContent = data['room'];
+   new_room_element.setAttribute("data-room_name", data['room']);
+   new_room_element.setAttribute("data-has_password", data['password']);
+   my_script.element_room_list.appendChild(new_room_element);
+   if (data['password']) {
+      new_room_element.addEventListener("click", function(ev) {
+         var prompt_password = prompt("Please enter the password for " + ev.currentTarget.getAttribute("data-room_name", ""));
+         send_join_room_request(ev.currentTarget.getAttribute("data-room_name"), prompt_password);
+      }, false);
+   } else {
+      new_room_element.addEventListener("click", function(ev) {
+         send_join_room_request(ev.currentTarget.getAttribute("data-room_name"));
+      }, false);
+   }
+});
+
+
+prompt_for_username();
